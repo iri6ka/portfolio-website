a44b402a2f4c5eed1dfc3fcad35c01097270ac36
iri6ka <ikonnova@gmail.com>
2021-06-26 15:06:25 +1000

    added React Dom, React Router Dom, React Icons, styled components, styled basic navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>Irina Konnova</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                            <NavLinks to="projects">Projects</NavLinks>
+                            <NavLinks to="contact">Contact</NavLinks>
+
+                        </NavItem>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar;
