42cfec3a3b88d91df987b2df92d46e6075297470
iri6ka <ikonnova@gmail.com>
2021-06-29 13:44:37 +1000

    fixed privacy of emailjs config via dotenv

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import emailjs from 'emailjs-com';
+require('dotenv').config();
 
-const Contact = () => {
-    return (
-        <div>
-            <form>
-                <label>First Name:<input type="text" name="name" /></label>
-                <label>Last Name:<input type="text" name="name" /></label>
-                <label>Company:<input type="text" name="name" /></label>
-                <label>Phone number:<input type="text" name="name" /></label>
-                <label>Phone number:<input type="text" name="name" /></label>
+export default function Contact() {
 
+    useEffect(() => {
 
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
+        console.log(process.env)
+    })
+
+    // console.log(process.env) ;
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('website_email_service', `${process.env.REACT_APP_OUR_TEMPLATE_ID}` , e.target, `${process.env.REACT_APP_YOUR_USER_ID}`)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <label>First Name</label>
+      <input type="text" className="form-control" name="firstName" />
+      <label>Last Name </label>
+      <input type="text" className="form-control" name="lastName" />
+      <label>Company</label>
+      <input type="text" className="form-control" name="company" />
+      <label>Position</label>
+      <input type="text" className="form-control" name="position" />
+      <label>Email</label>
+      <input type="text" className="form-control" name="email" />
+      <label>Phone Number</label>
+      <input type="text" className="form-control" name="phone" />
+      <label>Subject</label>
+      <input type="text" className="form-control" name="subject" />
+      <label>Message</label>
+      <textarea className="form-control" name="subject"></textarea>
+
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
-
-export default Contact
