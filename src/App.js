b027ba88b3796934f8752650cda78091f755d234
Irina K <72952520+iri6ka@users.noreply.github.com>
2021-06-29 14:13:35 +1000

    Contact form (#9)
    * asome minor changes + added a new file structure for a contact form
    
    * added dotenv dependency and  made emailjs token secure
    
    * fixed privacy of emailjs config via dotenv
    
    * removed console logs sfrom contact form
    
    * Delete .env

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages';
-// import About from './components/AboutSection'
-// import Skills from './components/Skills'
-// import Resume from './components/Resume'
+import Contact from './pages/contact'
+require('dotenv').config();
+
 
 const App = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={ Home } />
-                {/* <Route exact path="/" component={ About }  />
-                <Route exact path="/" component={ Skills } /> */}
+                <Route exact path="/contact" component={Contact} />
             </Switch>
         </Router>
     )
---
+++ src/pages/contact.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+require('dotenv').config();
+
+export default function Contact() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('website_email_service', `${process.env.REACT_APP_OUR_TEMPLATE_ID}` , e.target, `${process.env.REACT_APP_YOUR_USER_ID}`)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <label>First Name</label>
+      <input type="text" className="form-control" name="firstName" />
+      <label>Last Name </label>
+      <input type="text" className="form-control" name="lastName" />
+      <label>Company</label>
+      <input type="text" className="form-control" name="company" />
+      <label>Position</label>
+      <input type="text" className="form-control" name="position" />
+      <label>Email</label>
+      <input type="text" className="form-control" name="email" />
+      <label>Phone Number</label>
+      <input type="text" className="form-control" name="phone" />
+      <label>Subject</label>
+      <input type="text" className="form-control" name="subject" />
+      <label>Message</label>
+      <textarea className="form-control" name="subject"></textarea>
+
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
